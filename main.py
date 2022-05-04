26818e05af00052b8004dd20f9f97f9f0a80a035
cludlow1 <cwludlo@g.clemson.edu>
2022-05-04 00:49:39 -0400

    api is saying the token wasnt generated w read permission

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import pandas as pd
-from pandas.io.json import json_normalize
 
 load_dotenv()
 CLIENT_ID=os.getenv("CLIENT_ID")
@@ -14,35 +13,34 @@ REFRESH_TOKEN=os.getenv("REFRESH_TOKEN")
 payload= {
     'client_id': CLIENT_ID,
     'client_secret': CLIENT_SECRET,
-    'refresh_token': 'my_refresh_token',
-    'grant_type': REFRESH_TOKEN,
-    'f': 'json'
+    'refresh_token': REFRESH_TOKEN,
+    'grant_type': "refresh_token",
+    'f': 'json',
 }
 
 auth_url = "https://www.strava.com/oauth/token"
-activites_url = "https://www.strava.com/api/v3/athlete/activities"
+activities_url = "https://www.strava.com/api/v3/athlete/activities"
 
 
 def getMyData():
     print("Requesting Token...\n")
     res = requests.post(auth_url, data=payload, verify=False)
-    print(res.json())
     access_token = res.json()['access_token']
     header = {'Authorization': 'Bearer ' + access_token}
     param = {'per_page': 200, 'page': 1}
-    my_dataset = requests.get(activites_url, headers=header, params=param).json()
+    my_dataset = requests.get(activities_url, headers=header,params=param).json()
 
-    activities = json_normalize(my_dataset)
-
+    #activities = pd.json_normalize(my_dataset)
+    print(my_dataset)
 
     cols = ['name', 'upload_id', 'type', 'distance', 'moving_time',
              'average_speed', 'max_speed','total_elevation_gain',
              'start_date_local'
            ]
 
-    activities = activities[cols]
+    #activities = activities[cols]
 
-    activites.head(100)
+
 
 if __name__ == "__main__":
     getMyData()
